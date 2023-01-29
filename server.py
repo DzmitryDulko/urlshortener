0b57174fe28a98335c0673fe2b897d57fe20360c
dmitry <dmitry.dulko@gmail.com>
2023-01-29 12:05:16 -0600

    Switch to redis with counter

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import uvicorn
 import hashids
+import redis
 from fastapi import FastAPI, Request, Response
 from pydantic import BaseModel
 from urllib.parse import urlparse
@@ -12,21 +13,22 @@ class Item(BaseModel):
 # Create a hashids object to generate short URLs
 hashids = hashids.Hashids(salt='my_salt', min_length=5)
 
-# Dictionary to store mapping of short URLs to long URLs
-urls = {}
+r = redis.Redis(host='redis', port=6379, db=0, decode_responses=True)
 
 @app.post("/shorten")
 async def shorten_url(request: Request, item: Item):
     long_url = item.long_url
-    short_id = hashids.encode(len(urls))
+    counter_key = r.incr('counter_key')
+    short_id = hashids.encode(counter_key)
     parsed_url = urlparse(request.url._url)
     short_url = f"{parsed_url.scheme}://{parsed_url.netloc}/" + short_id
-    urls[short_id] = long_url
+    r.set(short_id, long_url)
     return {"short_url": short_url}
 
 @app.get("/{short_id}")
 async def redirect_url(short_id: str):
-    long_url = urls.get(short_id)
+    long_url = str(r.get(short_id))
+    print(long_url)
     if long_url:
         return Response(status_code=301, headers={"location": long_url})
     else:
